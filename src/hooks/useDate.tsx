6605a8d9fc842cb7482c465e0d8afce9469f7992
ysl0628 <yihsinlan@gmail.com>
2022-08-29 14:23:14 +0800

    feat: implement useDate
    - 取得現在時間 ( string )

---
+++ src/hooks/useDate.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function useDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const now = year + "/" + month + "/" + day + " " + hour + ":" + min;
+  return now;
+}
