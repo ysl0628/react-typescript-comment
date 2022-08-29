df022f843be0973c43cfcd5614cbf6a84d8e0d62
ysl0628 <yihsinlan@gmail.com>
2022-08-29 12:05:24 +0800

    feat: replace component name

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-export default function Comment() {
+interface PropType {
+  id: string;
+  info: Comment;
+}
+
+export default function Comments(props: PropType) {
+  const { id } = props;
   return (
     <div className="flex justify-center">
       <div className="flex mx-auto items-center justify-center shadow-lg mt-8 mb-4 max-w-lg">
         <form className="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
           <div className="flex flex-wrap -mx-3 mb-6">
-            <h2 className="px-4 pt-3 pb-2 text-gray-800 text-lg">ID:1</h2>
+            <h2 className="px-4 pt-3 pb-2 text-gray-800 text-lg">ID:{id}</h2>
             <div className="w-full md:w-full px-3 mb-2 mt-2">
               <div className="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"></div>
             </div>
