e0c8c8c6165ca80f26ffaebb85fbceb987c2af18
ysl0628 <yihsinlan@gmail.com>
2022-08-29 22:37:46 +0800

    feat: add replies index

--- src/components/ReplyItem.tsx
+++ src/components/ReplyItem.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Reply } from "../store/reducers/commentSlice";
 interface PropType {
-  replies: Reply;
+  replies: Reply | any;
 }
 
 export default function ReplyItem({ replies }: PropType) {
   return (
     <div className="w-full md:w-full px-3 mb-2 mt-2">
       <div className=" rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium focus:outline-none focus:bg-white">
-        {replies.content}
+        {replies[0].content}
       </div>
       <div className="flex items-start w-1/2 text-gray-700 mr-auto">
         <p className="text-xs md:text-sm pt-px align-middle">
-          Time: {replies.time}
+          Time: {replies[0].time}
         </p>
       </div>
     </div>
