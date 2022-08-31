2b9fe9cc9ab643313b062b08daf796d489b08e8b
ysl0628 <yihsinlan@gmail.com>
2022-08-31 12:04:27 +0800

    feat: style replyitem

--- src/components/ReplyItem.tsx
+++ src/components/ReplyItem.tsx
@@ -6,11 +6,15 @@ interface PropType {
 
 export default function ReplyItem({ replies }: PropType) {
   return (
-    <div className="w-full md:w-full px-3 mb-2 mt-2">
-      <div className=" rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium focus:outline-none focus:bg-white">
+    <div className="w-full md:w-full px-3 text-start mb-2 mt-2">
+      <div
+        className="border-b border-gray-400 leading-normal
+       resize-none w-full md:w-full pb-2 pt-1 font-medium break-words
+         focus:outline-none focus:bg-white"
+      >
         {replies.content}
       </div>
-      <div className="flex items-start w-1/2 text-gray-700 mr-auto">
+      <div className="flex items-start w-1/2 text-gray-700 mt-2 mr-auto">
         <p className="text-xs md:text-sm pt-px align-middle">
           Time: {replies.time}
         </p>
